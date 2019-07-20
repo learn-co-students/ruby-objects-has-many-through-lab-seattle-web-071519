e83092040aa6f32f0face120a888b615a1e89358
ayerest <irisiseve@gmail.com>
2019-07-19 17:28:20 -0700

    finished ruby objects has many through lab

---
+++ lib/patient.rb
@@ -0,0 +1,28 @@
+class Patient
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(doctor, date)
+        Appointment.new(date, self, doctor)
+    end
+    
+    def appointments
+        Appointment.all.select do |appointment|
+            appointment.patient == self
+        end
+    end
+
+    def doctors
+        self.appointments.collect do |appointment|
+            appointment.doctor
+        end
+    end
+end
